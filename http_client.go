8f24326884ce60eab5fd1a1b36e0f1e9d4443d5e
Xudong Guo <guoxd@jihulab.com>
2023-08-25 11:55:25 +0800

    fix

--- http_client.go
+++ http_client.go
@@ -38,47 +38,45 @@ func post(apiURL, token string, params map[string]interface{}, timeout time.Dura
 	return result, nil
 }
 
-// noinspection ALL
-func stream(apiURL, token string, params map[string]interface{}, timeout time.Duration) (*resty.Response, error) {
-	client := resty.New()
-	client.SetTimeout(timeout)
-	resp, err := client.R().
-		SetHeaders(headers).
-		SetHeader("Authorization", token).
-		SetQueryParam("stream", "true").
-		SetBody(params).
-		Post(apiURL)
-
-	if err != nil {
-		return nil, fmt.Errorf("请求异常：%w", err)
-	}
-	if resp.StatusCode() != http.StatusOK {
-		return nil, fmt.Errorf("响应异常：%s", resp.Body())
-	}
-
-	return resp, nil
-}
-
-// noinspection ALL
-func get(apiURL, token string, timeout time.Duration) (map[string]interface{}, error) {
-	client := resty.New()
-	client.SetTimeout(timeout)
-	resp, err := client.R().
-		SetHeaders(headers).
-		SetHeader("Authorization", token).
-		Get(apiURL)
-
-	if err != nil {
-		return nil, fmt.Errorf("请求异常：%w", err)
-	}
-	if resp.StatusCode() != http.StatusOK {
-		return nil, fmt.Errorf("响应异常：%s", resp.Body())
-	}
-	var result map[string]interface{}
-	err = json.Unmarshal(resp.Body(), &result)
-	if err != nil {
-		return nil, fmt.Errorf("JSON 解析异常：%w", err)
-	}
-
-	return result, nil
-}
+//func stream(apiURL, token string, params map[string]interface{}, timeout time.Duration) (*resty.Response, error) {
+//	client := resty.New()
+//	client.SetTimeout(timeout)
+//	resp, err := client.R().
+//		SetHeaders(headers).
+//		SetHeader("Authorization", token).
+//		SetQueryParam("stream", "true").
+//		SetBody(params).
+//		Post(apiURL)
+//
+//	if err != nil {
+//		return nil, fmt.Errorf("请求异常：%w", err)
+//	}
+//	if resp.StatusCode() != http.StatusOK {
+//		return nil, fmt.Errorf("响应异常：%s", resp.Body())
+//	}
+//
+//	return resp, nil
+//}
+//
+//func get(apiURL, token string, timeout time.Duration) (map[string]interface{}, error) {
+//	client := resty.New()
+//	client.SetTimeout(timeout)
+//	resp, err := client.R().
+//		SetHeaders(headers).
+//		SetHeader("Authorization", token).
+//		Get(apiURL)
+//
+//	if err != nil {
+//		return nil, fmt.Errorf("请求异常：%w", err)
+//	}
+//	if resp.StatusCode() != http.StatusOK {
+//		return nil, fmt.Errorf("响应异常：%s", resp.Body())
+//	}
+//	var result map[string]interface{}
+//	err = json.Unmarshal(resp.Body(), &result)
+//	if err != nil {
+//		return nil, fmt.Errorf("JSON 解析异常：%w", err)
+//	}
+//
+//	return result, nil
+//}
